ab696527bde5b0503b7328036dd1dc3e167e3b10
gary <gary.white@lodgit.com>
2021-11-30 13:35:21 +0100

    boilerplate

---
+++ index.js
@@ -0,0 +1,9 @@
+function fixTheMeerkat(arr) {
+    //your code here 
+}
+
+console.log(fixTheMeerkat(["tail", "body", "head"])); // Return: ["head", "body", "tail"]
+console.log(fixTheMeerkat(["tails", "body", "heads"])); // Return: ["heads", "body", "tails"]
+console.log(fixTheMeerkat(["bottom", "middle", "top"])); // Return: ["top", "middle", "bottom"]
+console.log(fixTheMeerkat(["lower legs", "torso", "upper legs"])); // Return: ["upper legs", "torso", "lower legs"]
+console.log(fixTheMeerkat(["ground", "rainbow", "sky"])); // Return: ["sky", "rainbow", "ground"]
