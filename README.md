ed40ce46e374da8ad5a54de296f84059a7c90699
gary <gary.white@lodgit.com>
2021-11-30 13:55:28 +0100

    code, readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Kata-My-head-is-at-the-wrong-end
+
+![Screen Image](pic.png)
+
+![Code Image](code.png)
--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function fixTheMeerkat(arr) {
 console.log(fixTheMeerkat(["tail", "body", "head"])); // Return: ["head", "body", "tail"]
 console.log(fixTheMeerkat(["tails", "body", "heads"])); // Return: ["heads", "body", "tails"]
 console.log(fixTheMeerkat(["bottom", "middle", "top"])); // Return: ["top", "middle", "bottom"]
-console.log(fixTheMeerkat(["lower legs", "torso", "upper legs"])); // Return: ["upper legs", "torso", "lower legs"]
+console.log(fixTheMeerkat(["lower legs", "torso", "upper legs"])); //["upper legs", "torso", "lower legs"]
 console.log(fixTheMeerkat(["ground", "rainbow", "sky"])); // Return: ["sky", "rainbow", "ground"]
